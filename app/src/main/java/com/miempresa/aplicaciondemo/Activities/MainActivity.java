3a40a8174e13e32b055936cb27cecabd334548ff
formador75 <misiontic.formador75@uis.edu.co>
2022-10-19 07:31:13 -0500

    Agrego adaptador y recicler view al MainActivity

--- app/src/main/java/com/miempresa/aplicaciondemo/Activities/MainActivity.java
+++ app/src/main/java/com/miempresa/aplicaciondemo/Activities/MainActivity.java
@@ -1,6 +1,9 @@
 package com.miempresa.aplicaciondemo.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,15 +11,40 @@ import android.view.View;
 import android.widget.Button;
 
 import com.miempresa.aplicaciondemo.R;
+import com.miempresa.aplicaciondemo.controladores.ControladorProductos;
+import com.miempresa.aplicaciondemo.modelos.Producto;
+import com.miempresa.aplicaciondemo.utilidades.AdaptadorProductos;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnIrAgregar;
+    private RecyclerView reciclerProductos;
+    private AdaptadorProductos adaptador;
+    private ControladorProductos controlador;
+    private List<Producto> listaProductos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnIrAgregar = findViewById(R.id.button);
+        reciclerProductos = findViewById(R.id.reciclerproductos);
+
+        controlador = new ControladorProductos(MainActivity.this);
+        listaProductos = new ArrayList<>();
+        adaptador = new AdaptadorProductos(listaProductos);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        reciclerProductos.setLayoutManager(mLayoutManager);
+        reciclerProductos.setItemAnimator(new DefaultItemAnimator());
+        reciclerProductos.setAdapter(adaptador);
+
+        listarProductos();
+
+
 
         btnIrAgregar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,8 +53,15 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+         }
 
+         public void listarProductos(){
 
+        if(adaptador == null) return;
+        listaProductos = controlador.listarProductos();
+        adaptador.setListaProductos(listaProductos);
+        adaptador.notifyDataSetChanged();
 
-    }
+         }
+
 }
